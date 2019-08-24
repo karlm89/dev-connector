dc8c49a9913adf058687870067b355c27da09e3b
Karl M <karl@fwdmotion.studio>
2019-08-23 19:21:26 -0500

    Finished e09

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult} = require('express-validator');
 
-// @route GET api/users
-// @desc Test Route
-// @access Public
-router.get('/', (req,res) => res.send('User route'));
+// @route   POST api/users
+// @desc    Register User
+// @access  Public
+router.post('/',[
+    // validation  Name
+    check('name', 'Name is required')
+        .not()
+        .isEmpty(),
+    // validation email
+    check('email', 'Please include a valid email address')
+        .isEmail(),
+    check('password', 'Please enter a password with 6 or more characters')
+        .isLength({
+            min: 6
+        })
+], (req,res) => { 
+    errors = validationResult(req); 
+    if(!errors.isEmpty() ) {
+        return res.status(400 ).json({ errors: errors.array() });
+    }
+
+    res.send('User route');
+});
 
 module.exports = router;
