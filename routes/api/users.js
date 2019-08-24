3c024fa8d94a145ad8b1f955a4d5e384bb4272de
Karl M <karl@fwdmotion.studio>
2019-08-24 05:22:19 -0500

    Finished e11

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs'); 
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult} = require('express-validator');
 
 const User = require('../../models/User');
@@ -56,9 +58,24 @@ router.post('/',[
 
         //Saves user to DB
         await user.save();
-        // Return JWT
 
-        res.send('User created successfully');
+        // Return JWT
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            { expiresIn: 360000 },
+            (err, token) => {
+                if(err) throw err;
+                res.json({token})
+            }
+        );
+
     } catch (err){
         console.error(err.message); 
         res.status(500).send('Server Error');
